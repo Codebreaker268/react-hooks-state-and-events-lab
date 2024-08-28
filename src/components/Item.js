e2e69bec19a143d7fcc13667c63d9d73499242f9
Godfrey Onyango <godiodhis268@gmail.com>
2024-08-28 07:47:26 +0300

    last

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [bought, setBought] = useState(false);
+  
+  // Define class based on the bought state
+  const className = bought ? "in-cart" : "";
+
+  // Define button text based on the bought state
+  const buttonText = bought ? "Remove from cart" : "Add to cart";
+
+  // Toggle the bought state
+  function handleBought() {
+    setBought(prevBought => !prevBought);
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleBought}>{buttonText}</button>
     </li>
   );
 }
